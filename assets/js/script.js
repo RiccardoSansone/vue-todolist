6c78941468ef9e1f5fb63d90e1efd0e0f96b2582
RiccardoSansone <r.sansone@gmail.com>
2023-09-13 15:09:24 +0200

    do milestone1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-
+            tasks: [
+                {
+                    text: 'comprare il pane',
+                    done: true
+                },
+                {
+                    text: 'comprare il latte',
+                    done: false
+                },
+                {
+                    text: 'comprare il pelato',
+                    done: true
+                }
+            ]
         }
     },
     methods: {
